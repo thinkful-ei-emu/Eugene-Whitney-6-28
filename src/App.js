b3231dad962f939bcc37851884ce6f0f8c30129a
Whitney Sam <whitneysamwallace@gmail.com>
2019-06-28 08:20:08 -0700

    Header and Feature compenent completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
+import Features from './components/Features';
 import './App.css';
 
 class App extends Component {
@@ -35,6 +37,7 @@ class App extends Component {
   }
 
   render() {
+    // Summary.js (on right of page)
     const summary = Object.keys(this.state.selected)
           .map(key => <div className="summary__option" key={key}>
             <div className="summary__option__label">{key}  </div>
@@ -45,48 +48,23 @@ class App extends Component {
             </div>
         </div>)
 
+    // Total.js (in Summary)
     const total = Object.keys(this.state.selected)
           .reduce((acc, curr) => acc + this.state.selected[curr].cost, 0);    
 
-
-    const features = Object.keys(this.props.features)
-          .map(key => {
-            const options = this.props.features[key].map((item, index) => {
-              const selectedClass = item.name === this.state.selected[key].name ? 'feature__selected' : '';
-              const featureClass = 'feature__option ' + selectedClass;
-              return <li key={index} className="feature__item">
-                <div className={featureClass}
-                  
-                  onClick={e => this.updateFeature(key, item)}>
-                    { item.name }
-                    ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                      .format(item.cost) })
-                </div>
-              </li>
-            });
-
-            return <div className="feature" key={key}>
-              <div className="feature__name">{key}</div>
-              <ul className="feature__list">
-                { options }
-              </ul>
-            </div>
-          });      
+        
 
     return (
       <div className="App">
-        <header>
-          <h1>ELF Computing</h1>
-          <h3>Laptops</h3>
-          <h5>Customize your laptop</h5>  
-        </header>      
+      {/* Header.js */}
+      <Header />
+      
         <main>
-          <section className="main__form">
-            <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
-            { features }
-          </section>
+          <Features features={this.props.features} state={this.state} updateFeature={(feature, newValue) => this.updateFeature(feature, newValue)}/>
+          
           <section className="main__summary">
             <h3>NEW GREENLEAF 2018</h3>
+            {/* Summary.js */}
             {summary}
             <div className="summary__total">
               <div className="summary__total__label">Your Price: </div>
---
+++ src/components/Feature.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../App.css';
+
+function Feature() {
+
+  return (
+    
+  )
+}
+
+export default Feature
